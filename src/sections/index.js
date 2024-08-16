fe4606b349031e902b8e344fa990ed5ed4a71836
marshadn <muhamedmarshad18@gmail.com>
2024-08-16 22:04:27 +0530

    Add Files

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+
+
+export {
+    Hero,
+    PopularProducts,
+    Footer,
+    SuperQuality,
+    Services,
+    Subscribe,
+    CustomerReviews,
+    SpecialOffer
+};
